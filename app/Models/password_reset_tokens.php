3e9e8dd1dfa0145e6c8410e54cc1e4d92cb57af8
sheikdaw <52328052+sheikdaw@users.noreply.github.com>
2024-12-17 20:44:03 +0530

    main

--- app/Models/password_reset_tokens.php
+++ app/Models/password_reset_tokens.php
@@ -8,11 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class password_reset_tokens extends Model
 {
     use HasFactory;
-    public $timestamps = false;
-    protected $table = 'password_reset_tokens'; // Specify the table name
+
+    protected $table = 'password_reset_tokens';
+
+    public $timestamps = false; // Disable timestamps if they're not used
 
     protected $fillable = [
         'email',
         'token',
-    ]; // Define fillable fields
+    ];
+
+    protected $primaryKey = 'email'; // Use email as the primary key
+    public $incrementing = false; // Set to false if the primary key is not an integer
+    protected $keyType = 'string';
 }
