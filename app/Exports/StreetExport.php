fd71e297f90bcad738f549249d7e11427c6d7993
sheikdaw <52328052+sheikdaw@users.noreply.github.com>
2024-12-23 15:25:19 +0530

    main

--- app/Exports/StreetExport.php
+++ app/Exports/StreetExport.php
@@ -4,8 +4,11 @@ namespace App\Exports;
 
 use Illuminate\Contracts\View\View;
 use Maatwebsite\Excel\Concerns\FromView;
+use Maatwebsite\Excel\Concerns\WithTitle;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
-class StreetExport implements FromView
+class StreetExport implements FromView, WithTitle, WithStyles
 {
     protected $roadName;
     protected $data;
@@ -22,5 +25,23 @@ class StreetExport implements FromView
             'roadName' => $this->roadName,
             'data' => $this->data,
         ]);
+    }
+
+    public function title(): string
+    {
+        return substr(preg_replace('/[^A-Za-z0-9 _-]/', '', $this->roadName), 0, 31);
+    }
+
+    public function styles(Worksheet $sheet)
+    {
+        // Set auto width for all columns
+        foreach (range('A', $sheet->getHighestColumn()) as $col) {
+            $sheet->getColumnDimension($col)->setAutoSize(true);
+        }
+
+        // Optional: Set header style
+        return [
+            1 => ['font' => ['bold' => true]], // First row bold
+        ];
     }
 }
--- resources/views/exports/streetwise.blade.php
+++ resources/views/exports/streetwise.blade.php
@@ -1,20 +1,27 @@
 <table>
     <thead>
         <tr>
-            <th colspan="3" style="text-align: center; font-weight: bold;">Road: {{ $roadName }}</th>
-        </tr>
-        <tr>
+            <th>Assessment</th>
+            <th>Old Assessment</th>
+            <th>Owner Name</th>
+            <th>Mobile No</th>
+            <th>Usage</th>
             <th>Old Door No</th>
             <th>New Door No</th>
-            <!-- Add other columns as necessary -->
+            <th>Road Name</th>
         </tr>
     </thead>
     <tbody>
         @foreach ($data as $row)
             <tr>
+                <td>{{ $row->assessment }}</td>
+                <td>{{ $row->old_assessment }}</td>
+                <td>{{ $row->owner_name }}</td>
+                <td>{{ $row->phone }}</td>
+                <td>{{ $row->usage }}</td>
                 <td>{{ $row->old_door_no }}</td>
                 <td>{{ $row->new_door_no }}</td>
-                <!-- Add other fields as necessary -->
+                <td>{{ $row->road_name }}</td>
             </tr>
         @endforeach
     </tbody>
