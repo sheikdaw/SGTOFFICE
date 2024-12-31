b909f73fe30d7d644245fdea3196ef28dda8fcf9
sheikdaw <52328052+sheikdaw@users.noreply.github.com>
2024-12-31 11:24:48 +0530

    main

--- app/Exports/missingBillExport.php
+++ app/Exports/missingBillExport.php
@@ -5,8 +5,9 @@ namespace App\Exports;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class missingBillExport implements FromCollection
+class MissingBillExport implements FromCollection, WithHeadings
 {
     protected $data;
 
@@ -17,12 +18,35 @@ class missingBillExport implements FromCollection
 
     public function collection()
     {
-        // Assuming `assessment` and `pointdata` are columns in the respective tables
         return DB::table($this->data->mis)
+            ->select([
+                'assessment',
+                'old_assessment',
+                'owner_name',
+                'old_door_no',
+                'phone',
+                'road_name',
+                'building_usage',
+
+            ])
             ->whereNotIn('assessment', function ($query) {
                 $query->select('assessment')
                     ->from($this->data->pointdata);
             })
             ->get();
+    }
+
+    public function headings(): array
+    {
+        return [
+            'Assessment',
+            'Old Assessment',
+            'Owner Name',
+            'Old Door No',
+            'Phone',
+            'Road Name',
+            'Building Usage',
+
+        ];
     }
 }
