c58bef6c789e8564263cd53c31c2e2f961a3a84f
sheikdaw <52328052+sheikdaw@users.noreply.github.com>
2025-01-17 14:05:58 +0530

    main

--- app/Exports/UsageAreaVariationExport.php
+++ app/Exports/UsageAreaVariationExport.php
@@ -5,27 +5,48 @@ namespace App\Exports;
 use Maatwebsite\Excel\Concerns\WithMultipleSheets;
 
 
+// class UsageAreaVariationExport implements WithMultipleSheets
+// {
+//     protected $areavariation;
+//     protected $usageVariation;
+//     protected $AbstractExport;
+//     protected $rname;
+
+//     public function __construct($areavariation, array $usageVariation, $rname)
+//     {
+//         // $this->AbstractExport = $AbstractExport;
+//         $this->areavariation = $areavariation;
+//         $this->usageVariation = $usageVariation;
+//         $this->rname = $rname; // Correct assignment
+//     }
+
+//     public function sheets(): array
+//     {
+//         return [
+//             new AreaVariationExport($this->areavariation, $this->rname),
+//             new UsageVariationExport($this->usageVariation, $this->rname),
+//             // new AbstractExport($this->AbstractExport, $this->rname),
+//         ];
+//     }
+// }
 class UsageAreaVariationExport implements WithMultipleSheets
 {
-    protected $areavariation;
-    protected $usageVariation;
-    protected $AbstractExport;
-    protected $rname;
+    protected $usageData;
+    protected $areaData;
+    protected $roadName;
 
-    public function __construct($areavariation, array $usageVariation, $rname)
+    public function __construct($usageData, $areaData, $roadName)
     {
-        // $this->AbstractExport = $AbstractExport;
-        $this->areavariation = $areavariation;
-        $this->usageVariation = $usageVariation;
-        $this->rname = $rname; // Correct assignment
+        $this->usageData = $usageData;
+        $this->areaData = $areaData;
+        $this->roadName = $roadName;
     }
 
     public function sheets(): array
     {
         return [
-            new AreaVariationExport($this->areavariation, $this->rname),
-            new UsageVariationExport($this->usageVariation, $this->rname),
-            // new AbstractExport($this->AbstractExport, $this->rname),
+            new SingleSheetExport($this->usageData, "Usage Variation"),
+            new SingleSheetExport($this->areaData, "Area Variation"),
         ];
     }
 }
